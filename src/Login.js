e866f1a4de4316f06818f468ff7c37b93d8abb7d
Ben <benvanderhoeven@gmail.com>
2023-03-05 23:02:21 -0700

    restart

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client' 
 const App = () => {           
     return (                     
-        <div>                             
-            <h2>Hello World!</h2>            
-        </div> ) }
+        <>
+        <h4>Register</h4>
+        <form>
+          <input type="text" placeholder="username"></input>
+          <input type="password" placeholder="password"></input>
+          <button type="submit">Register</button>
+        </form>
+      </> ) }
 const appElement = document.getElementById("app")
 const root = createRoot(appElement)
 root.render(<BrowserRouter><App /></BrowserRouter>);
---
+++ src/Signup.js
@@ -0,0 +1,33 @@
+import { createRoot } from 'react-dom/client' 
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+
+
+
+
+//const API_URL
+
+
+
+const Signup = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+//async function
+
+// try {
+    //const respone = await fetch ()
+
+return (
+    <>
+      <h4>Register</h4>
+      <form>
+        <input type="text" placeholder="username"></input>
+        <input type="password" placeholder="password"></input>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
+
+export default Signup;
